9c2a1bea22d3a7a24d8d20674f39279059dd4dcf
Sauilitired <citymonstret@hotmail.com>
2014-05-21 21:14:19 +0200

    Started working on DBC integration.
    DBC Integration is coming soon.

---
+++ armory/core/dbc/readme.php
@@ -0,0 +1,8 @@
+<?php
+	die("This is just a readme...");
+?>
+
+	//READ THIS
+		Put your .dbc files in here (this will allow the armory to read in custom area names,
+		custom titles and things like that.
+	//
